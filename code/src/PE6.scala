e81c4aab673574b582bf217024a7b84a3f7c3a23
krishna <krishnakalyan3@gmail.com>
2015-10-21 00:10:52 +0200

    Commit Date Wed Oct 21 00:10:51 CEST 2015

--- code/src/PE6.scala
+++ code/src/PE6.scala
@@ -2,7 +2,7 @@
  * Created by krishna on 21/10/15.
  */
 object euler6 extends App{
-  val sumSquares = (1 to 100).map(n => n * n).sum
+  val sumSquares = (1 to 100).foldLeft(0)((a,b) => a+b)
   val sum = (1 to 100).sum
   val squareSum = sum*sum
   val answer =  squareSum - sumSquares
